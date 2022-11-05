bdc7ad46eca9cf969fe21c60973e34902310e081
DeazStar <deadfromtheborn@gmail.com>
2022-11-05 18:53:12 +0300

    fix

--- sort.h
+++ sort.h
@@ -23,7 +23,7 @@ void bubble_sort(int *array, size_t size);
 void swap(int *x, int *y);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_recurssion(int *array, int low, int high, size_t size);
-int partition(int *array, int low, int high, size_t size);
+void lomuto_sort(int *array, size_t size, int left, int right);
+int lomuto_partition(int *array, size_t size, int left, int right);
 
 #endif
