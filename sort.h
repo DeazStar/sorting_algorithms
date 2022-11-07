3388891dd435ae567e91d218d0d37a0e325b72a0
DeazStar <deadfromtheborn@gmail.com>
2022-11-07 22:30:40 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -33,5 +33,6 @@ void _swap(int *array, int i, int j);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void _swap_list(listint_t **node, listint_t **list);
+void counting_sort(int *array, size_t size);
 
 #endif
