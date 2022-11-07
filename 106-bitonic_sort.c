547c425589738d6d8e1fc41c3771ac18e5dee54d
DeazStar <deadfromtheborn@gmail.com>
2022-11-07 22:44:45 +0300

    106-bitonic_sort.c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -25,7 +25,7 @@ void swap_2(int arr[], int item1, int item2, int order)
  * @nelemnt: elements number
  * @order: 1: ascending order, 0 descending order
  */
-void merge(int arr[], int low, int nelemnt, int order)
+void merge_order(int arr[], int low, int nelemnt, int order)
 {
 	int mid, i;
 
@@ -34,8 +34,8 @@ void merge(int arr[], int low, int nelemnt, int order)
 		mid = nelemnt / 2;
 		for (i = low; i < low + mid; i++)
 			swap_2(arr, i, i + mid, order);
-		merge(arr, low, mid, order);
-		merge(arr, low + mid, mid, order);
+		merge_order(arr, low, mid, order);
+		merge_order(arr, low + mid, mid, order);
 	}
 }
 /**
@@ -65,7 +65,7 @@ void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
 		mid = nelemnt / 2;
 		bitonicsort(arr, low, mid, 1, size);
 		bitonicsort(arr, low + mid, mid, 0, size);
-		merge(arr, low, nelemnt, order);
+		merge_order(arr, low, nelemnt, order);
 		if (order <= 0)
 		{
 			printf("Result [%i/%i] (DOWN):\n", nelemnt, size);
